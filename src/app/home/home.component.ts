114ca7924abd9afd2cc43caaa4f2b5fb1ef184d1
Decebel Geduriagao <Decebel Geduriagao>
2018-02-16 21:47:09 +0800

    fixed syntax error

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -17,7 +17,7 @@ import { DataService } from '../data.service';
             style({opacity: 0, transform: 'translateY(-75%)', offset: 0}),
             style({opacity: .5, transform: 'translateY(35px)',  offset: 0.3}),
             style({opacity: 1, transform: 'translateY(0)',     offset: 1.0}),
-          ]))]), {optional: true})
+          ]))]), {optional: true}),
 
         query(':leave', stagger('300ms', [
           animate('.6s ease-out', keyframes([
